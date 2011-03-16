3be51ac06b450469797b5b600ad62f275d0d8da8
Richard Lucas <rlucas11@yahoo.com>
2011-03-16 06:53:20 -0400

    Move files from original setup to project folder

--- lib/utilities.R
+++ lib/utilities.R
@@ -5,3 +5,11 @@ clean.variable.name <- function(variable.name)
   variable.name <- gsub('\\s+', '.', variable.name, perl = TRUE)
   return(variable.name)
 }
+
+### r to Z functions for aggregating correlations
+rtoZ <- function(r) {
+  return(.5*(log(1+r)-log(1-r)))
+}
+ZtoR <- function(Z) {
+  return((exp(2*Z)-1)/(exp(2*Z)+1))
+}
