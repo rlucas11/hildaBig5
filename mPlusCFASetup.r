60d6355283ae82ac9bb57d819f52533088171845
Richard Lucas <rlucas11@yahoo.com>
2011-03-16 10:40:16 -0400

    new analyses

--- mPlusCFASetup.r
+++ mPlusCFASetup.r
@@ -28,11 +28,35 @@ runModels("reports/Auto/Model1",Mplus_command="wine Mplus.exe")
 summaryStats1 <- extractModelSummaries("reports/Auto/Model1")
 showSummaryTable(summaryStats1)
 
-extractModelSummaries("reports/Auto",recursive=TRUE,filefilter=".*extraversion*")
-extractModelSummaries("reports/Auto",recursive=TRUE,filefilter=".*agreeableness*")
-extractModelSummaries("reports/Auto",recursive=TRUE,filefilter=".*conscientiousness*")
-extractModelSummaries("reports/Auto",recursive=TRUE,filefilter=".*neuroticism*")
-extractModelSummaries("reports/Auto",recursive=TRUE,filefilter=".*openness*")
+eFit <- extractModelSummaries("reports/Auto",recursive=TRUE,filefilter=".*extraversion*")
+aFit <- extractModelSummaries("reports/Auto",recursive=TRUE,filefilter=".*agreeableness*")
+cFit <- extractModelSummaries("reports/Auto",recursive=TRUE,filefilter=".*conscientiousness*")
+nFit <- extractModelSummaries("reports/Auto",recursive=TRUE,filefilter=".*neuroticism*")
+oFit <- extractModelSummaries("reports/Auto",recursive=TRUE,filefilter=".*openness*")
+
+###Create Fit Summary Table
+eSummary <- eFit[,c("ChiSqM_Value","ChiSqM_DF","CFI","TLI","RMSEA_Estimate","SRMR")]
+aSummary <- aFit[,c("ChiSqM_Value","ChiSqM_DF","CFI","TLI","RMSEA_Estimate","SRMR")]
+cSummary <- cFit[,c("ChiSqM_Value","ChiSqM_DF","CFI","TLI","RMSEA_Estimate","SRMR")]
+nSummary <- nFit[,c("ChiSqM_Value","ChiSqM_DF","CFI","TLI","RMSEA_Estimate","SRMR")]
+oSummary <- oFit[,c("ChiSqM_Value","ChiSqM_DF","CFI","TLI","RMSEA_Estimate","SRMR")]
+
+dimnames(eSummary) <- list(c("Model 1", "Model 2", "Model 3", "Model 4", "Model 5"),
+                           c("Chi-Square", "DF", "CFI", "TLI", "RMSEA", "SRMR"))
+dimnames(aSummary) <- list(c("Model 1", "Model 2", "Model 3", "Model 4", "Model 5"),
+                           c("Chi-Square", "DF", "CFI", "TLI", "RMSEA", "SRMR"))
+dimnames(cSummary) <- list(c("Model 1", "Model 2", "Model 3", "Model 4", "Model 5"),
+                           c("Chi-Square", "DF", "CFI", "TLI", "RMSEA", "SRMR"))
+dimnames(nSummary) <- list(c("Model 1", "Model 2", "Model 3", "Model 4", "Model 5"),
+                           c("Chi-Square", "DF", "CFI", "TLI", "RMSEA", "SRMR"))
+dimnames(oSummary) <- list(c("Model 1", "Model 2", "Model 3", "Model 4", "Model 5"),
+                           c("Chi-Square", "DF", "CFI", "TLI", "RMSEA", "SRMR"))
+
+xtable(eSummary, caption="Fit Indexes for Measurement Invariance CFA Models")
+xtable(aSummary, caption="Fit Indexes for Measurement Invariance CFA Models")
+xtable(cSummary, caption="Fit Indexes for Measurement Invariance CFA Models")
+xtable(nSummary, caption="Fit Indexes for Measurement Invariance CFA Models")
+xtable(oSummary, caption="Fit Indexes for Measurement Invariance CFA Models")
 
 ###Means
 extParam <- extractModelParameters("reports/Auto/Model5",filefilter=".*extraversion*")
